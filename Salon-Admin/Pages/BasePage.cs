9fa7f127f31e78f99091497897ba2e10c49f5d00
ThanhPhong278 <phongvo2728@gmail.com>
2024-11-12 23:26:01 +0700

    Add project files.

---
+++ Salon-Admin/Pages/BasePage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace Salon_Admin.Pages
+{
+    public class BasePage
+    {
+        protected readonly IPage page;
+
+        public BasePage(IPage page)
+        {
+            this.page = page;
+        }
+    }
+}
